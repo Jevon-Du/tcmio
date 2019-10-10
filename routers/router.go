e33e5cb979a5326c0233227fc91e6b76d9021d5b
nanomolar <zhihongliu2011@gmail.com>
2019-10-10 22:19:24 +0800

    add routers

--- routers/router.go
+++ routers/router.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{}, "get:Index")
 
 	beego.Router("/index", &controllers.MainController{}, "get:Index")
 	beego.Router("/help", &controllers.MainController{}, "get:Help")
@@ -15,19 +15,24 @@ func init() {
 	beego.Router("/structure", &controllers.MainController{}, "get:Structure")
 	beego.Router("/moa", &controllers.MainController{}, "get:MOA")
 
+	beego.Router("/target", &controllers.MainController{}, "get:Target")
 	beego.Router("/targets", &controllers.MainController{}, "get:ListTargets")
 	beego.Router("/targets/:id([0-9]+)", &controllers.MainController{}, "get:DetailTarget")
 
+	beego.Router("/ligand", &controllers.MainController{}, "get:Ligand")
 	beego.Router("/ligands", &controllers.MainController{}, "get:ListLigands")
 	beego.Router("/ligands/:id([0-9]+)", &controllers.MainController{}, "get:DetailLigand")
 	beego.Router("/ligands/structure/:method", &controllers.MainController{}, "get:SearchLigands")
 
+	beego.Router("/ingredient", &controllers.MainController{}, "get:Ingredient")
 	beego.Router("/ingredients", &controllers.MainController{}, "get:ListIngredients")
 	beego.Router("/ingredients/:id([0-9]+)", &controllers.MainController{}, "get:DetailIngredient")
 
+	beego.Router("/tcm", &controllers.MainController{}, "get:TCM")
 	beego.Router("/tcms", &controllers.MainController{}, "get:ListTCMs")
 	beego.Router("/tcms/:id([0-9]+)", &controllers.MainController{}, "get:DetailTCM")
 
+	beego.Router("/prescription", &controllers.MainController{}, "get:Prescription")
 	beego.Router("/prescriptions", &controllers.MainController{}, "get:ListPrescriptions")
 	beego.Router("/prescriptions/:id([0-9]+)", &controllers.MainController{}, "get:DetailPrescription")
 
