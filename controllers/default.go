fc6218f4ed30e3a132fad445499e21f4f0c3ec96
nanomolar <zhihongliu2011@gmail.com>
2019-10-12 18:04:06 +0800

    change routers

--- controllers/default.go
+++ controllers/default.go
@@ -30,24 +30,8 @@ func (this *MainController) Browse() {
 	this.TplName = "browse.html"
 }
 
-func (this *MainController) Target() {
-	this.TplName = "target.html"
-}
-
-func (this *MainController) Ligand() {
-	this.TplName = "ligand.html"
-}
-
-func (this *MainController) Ingredient() {
-	this.TplName = "ingredient.html"
-}
-
-func (this *MainController) TCM() {
-	this.TplName = "tcm.html"
-}
-
-func (this *MainController) Prescription() {
-	this.TplName = "prescription.html"
+func (this *MainController) Detail() {
+	this.TplName = "detail.html"
 }
 
 func (this *MainController) Structure() {
