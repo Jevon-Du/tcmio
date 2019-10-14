a269d925317c862b431e0b375529e477afa09aec
nanomolar <zhihongliu2011@gmail.com>
2019-10-14 10:22:18 +0800

    add data process

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	_ "tcmio/routers"
+	"tcmio/util"
 
 	"github.com/astaxie/beego"
 )
 
 func main() {
 	//util.SDFToDatabase("E:/GIM/TCM/TCMIO/data/all-ligands1.sdf")
+	util.DataProcess()
 	beego.Run()
 }
