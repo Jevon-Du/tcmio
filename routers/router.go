93f0af115fb4ee76d801c7ebabaebfd3d193154c
nanomolar <zhihongliu2011@gmail.com>
2019-10-24 18:05:04 +0800

    add network analysis

--- routers/router.go
+++ routers/router.go
@@ -11,32 +11,34 @@ func init() {
 
 	beego.Router("/index", &controllers.MainController{}, "get:Index")
 	beego.Router("/help", &controllers.MainController{}, "get:Help")
-	beego.Router("/browse", &controllers.MainController{}, "get:Browse")
+	beego.Router("/browse/:category:string", &controllers.MainController{}, "get:Browse")
 	beego.Router("/structure", &controllers.MainController{}, "get:Structure")
 	beego.Router("/moa", &controllers.MainController{}, "get:MOA")
 
 	beego.Router("/targets", &controllers.MainController{}, "get:ListTargets")
 	beego.Router("/targets/:id([0-9]+)", &controllers.MainController{}, "get:Detail")
-	beego.Router("/targets/:id([0-9]+)?data=json", &controllers.MainController{}, "get:DetailTarget")
+	beego.Router("/targets/:id([0-9]+)/json", &controllers.MainController{}, "get:DetailTarget")
 
 	beego.Router("/ligands", &controllers.MainController{}, "get:ListLigands")
 	beego.Router("/ligands/:id([0-9]+)", &controllers.MainController{}, "get:Detail")
-	beego.Router("/ligands/:id([0-9]+)?data=json", &controllers.MainController{}, "get:DetailLigand")
+	beego.Router("/ligands/:id([0-9]+)/json", &controllers.MainController{}, "get:DetailLigand")
 	beego.Router("/ligands/structure/:method", &controllers.MainController{}, "get:SearchLigands")
 
 	beego.Router("/ingredients", &controllers.MainController{}, "get:ListIngredients")
 	beego.Router("/ingredients/:id([0-9]+)", &controllers.MainController{}, "get:Detail")
-	beego.Router("/ingredients/:id([0-9]+)?data=json", &controllers.MainController{}, "get:DetailIngredient")
+	beego.Router("/ingredients/:id([0-9]+)/json", &controllers.MainController{}, "get:DetailIngredient")
 
 	beego.Router("/tcms", &controllers.MainController{}, "get:ListTCMs")
 	beego.Router("/tcms/:id([0-9]+)", &controllers.MainController{}, "get:Detail")
-	beego.Router("/tcms/:id([0-9]+)?data=json", &controllers.MainController{}, "get:DetailTCM")
+	beego.Router("/tcms/:id([0-9]+)/json", &controllers.MainController{}, "get:DetailTCM")
+	beego.Router("/tcms/network", &controllers.MainController{}, "*:AnalyzeTCMs")
 
 	beego.Router("/prescriptions", &controllers.MainController{}, "get:ListPrescriptions")
 	beego.Router("/prescriptions/:id([0-9]+)", &controllers.MainController{}, "get:Detail")
-	beego.Router("/prescriptions/:id([0-9]+)?data=json", &controllers.MainController{}, "get:DetailPrescription")
+	beego.Router("/prescriptions/:id([0-9]+)/json", &controllers.MainController{}, "get:DetailPrescription")
+	beego.Router("/prescriptions/network", &controllers.MainController{}, "*:AnalyzePrescription")
 
-	//beego.Router("/structure/analysis", &controllers.MainController{}, "post:StructureSearch")
-	//beego.Router("/moa/analysis", &controllers.MainController{}, "post:MOA")
+	beego.Router("/structure/ligand/:method", &controllers.MainController{}, "*:StructureSearchLigand")
+	beego.Router("/structure/ingredient/:method", &controllers.MainController{}, "*:StructureSearchIngredient")
 
 }
